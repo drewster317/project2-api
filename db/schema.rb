15fec02dc55f821cead41ec7d92cbc487eb2f80b
Drewster317 <drewster317@gmail.com>
2015-11-10 13:24:20 -0500

    DELETE tables and ADD roles table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110145734) do
+ActiveRecord::Schema.define(version: 20151110181902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,19 +31,14 @@ ActiveRecord::Schema.define(version: 20151110145734) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "instructors", force: :cascade do |t|
-    t.string   "given_name"
-    t.string   "surname"
+  create_table "roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "students", force: :cascade do |t|
-    t.string   "given_name"
-    t.string   "surname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  add_index "roles", ["user_id"], name: "index_roles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -56,4 +51,5 @@ ActiveRecord::Schema.define(version: 20151110145734) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
+  add_foreign_key "roles", "users"
 end
