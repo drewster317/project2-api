d3b84c267ca18cc4e90426a42cf15fca4c8e7d6f
Drewster317 <drewster317@gmail.com>
2015-12-17 11:32:21 -0500

    ASSOCIATED active help items with instructors on the front-end

--- app/serializers/help_item_serializer.rb
+++ app/serializers/help_item_serializer.rb
@@ -1,5 +1,5 @@
 class HelpItemSerializer < ActiveModel::Serializer
-  attributes :comment, :status,  :student_id, :student_first_name, :student_last_name, :instructor_id, :instructor_name
+  attributes :comment, :status,  :student_id, :student_first_name, :student_last_name, :instructor_id, :instructor_full_name, :instructor_first_name
 
   def student_id
     object.student.id
@@ -17,7 +17,12 @@ class HelpItemSerializer < ActiveModel::Serializer
     object.instructor.id if object.instructor
   end
 
-  def instructor_name
+  def instructor_full_name
     object.instructor.profile.full_name if object.instructor
   end
+
+  def instructor_first_name
+    object.instructor.profile.first_name if object.instructor
+  end
+
 end
